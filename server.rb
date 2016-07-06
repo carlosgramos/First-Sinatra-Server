6696249dc3d790a476aaee95e7a7ac3237febf8b
Carlos Ramos <carlosr86@hotmail.com>
2016-07-06 14:34:08 -0400

    first commit

---
+++ server.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+
+#as a server, when receiving the get method on the root path, respond with the
+#string hello world
+get '/' do
+  "Hello world" #This is a Ruby method, the Hello World is implied
+end
+
+get "/sinatra" do
+	"Hello Sinatra"
+end
