eaf5b49b66347252053f3b0fdc1bc1563d0cdb20
carlosgramos <carlosr86@hotmail.com>
2016-07-12 17:08:13 -0400

    Fixing the bad instructions given by Goodmeasure

--- server.rb
+++ server.rb
@@ -2,12 +2,10 @@ require 'sinatra'
 #as a server, when receiving the get method on the root path, respond with the
 #string hello world
 get '/' do
-  "Hello world"#This is a Ruby method, the Hello World is implied
+  #"Hello world"#This is a Ruby method, the Hello World is implied
+  File.read(File.join('public', 'hello.txt'))
 end
 
 get "/sinatra" do
 	"Hello Sinatra"
-end
-
-get "/hello_world" do
 end
