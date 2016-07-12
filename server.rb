6ff025f758b4d770df57df9bcca203c812eb712f
carlosgramos <carlosr86@hotmail.com>
2016-07-12 16:21:34 -0400

    Rerouted 'Hello World.txt' to '/'

--- server.rb
+++ server.rb
@@ -1,11 +1,13 @@
 require 'sinatra'
-
 #as a server, when receiving the get method on the root path, respond with the
 #string hello world
 get '/' do
-  "Hello world" #This is a Ruby method, the Hello World is implied
+  "Hello world"#This is a Ruby method, the Hello World is implied
 end
 
 get "/sinatra" do
 	"Hello Sinatra"
+end
+
+get "/hello_world" do
 end
